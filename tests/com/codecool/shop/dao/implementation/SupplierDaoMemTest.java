8971921cfe0517212aec13b63799d29dc4c043fd
peterosz <itharius@gmail.com>
2017-10-05 11:37:54 +0200

    test folder created

---
+++ tests/com/codecool/shop/dao/implementation/SupplierDaoMemTest.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.dao.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupplierDaoMemTest {
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+}
